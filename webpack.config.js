3f31f13497255e248375dedfb91c16d1db33d322
pookie86 <lalaina36@gmail.com>
2019-05-29 16:40:01 +0200

    :memo: Exercice > Update > Webpack > Serveur local

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+    const path = require("path");
+
+  module.exports = {
+
+    mode: 'production',
+    entry: "./src/script.js",
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "./bundle.js"
+    },
+    
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        open: true
+    },
+
+
+}
+
